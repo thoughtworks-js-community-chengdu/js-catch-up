923d9c4bb8816f61859321d20d03a7cab807da7a
Jinke.Li <1359518268@qq.com>
2019-05-23 15:19:45 +0800

    feat: Complete

--- study/babel插件编写/JinkeLi/index.js
+++ study/babel插件编写/JinkeLi/index.js
@@ -8,12 +8,40 @@
     const desc = 'my name is ' + name
  */
 
-export default function({ types: t }) {
+export default function ({ types: t }) {
   return {
     visitor: {
-      TemplateElement(path, state) {
-        // TODO:
-        console.log(path)
+      TemplateLiteral(path) {
+        const { node } = path
+        let params
+        let str
+        if (
+          node &&
+          node.expressions &&
+          node.quasis &&
+          node.expressions.length >= 1 &&
+          node.quasis.length >= 1 &&
+          node.expressions.every((expression) => t.isIdentifier(expression))
+        ) {
+          params = node.expressions.reduce((str, { name }, i) => {
+            if (i > 0) {
+              str += " + "
+            }
+            str += name
+            return str
+          }, "")
+
+          const element = node.quasis[0]
+          if(t.isTemplateElement(element)) {
+            str = element.value.raw
+          }
+        }
+
+        if(!params) {
+          return
+        }
+        const result = `'${str}' + ${params}`
+        path.replaceWithSourceString(result)
       },
     }
   };
--- study/babel插件编写/JinkeLi/index.test.js
+++ study/babel插件编写/JinkeLi/index.test.js
@@ -7,18 +7,36 @@ describe('test', () => {
       return this.replace(/\s/g, "")
     }
   })
-  it('test expression', () => {
+
+  it('test single parameter', () => {
     const code = `
-      const name = "bob"
-      const desc = \`my name is ${name}\`
+      const name = "bob";
+      const desc = \`my name is \${name}\ age\`;
     `
     const result = transform(code, {
       plugins: [Plugin]
     })
 
     expect(result.code.trimAll()).toEqual(`
-      const name = "bob"
-      const desc = 'my name is ' + ${name}
+      const name = "bob";
+      const desc = 'my name is ' + name;
+    `.trimAll())
+  })
+
+  it('test multiple parameters', () => {
+    const code = `
+      const name = "bob";
+      const age = 18;
+      const desc = \`my name is \${name}\ age is \${age}\`;
+    `
+    const result = transform(code, {
+      plugins: [Plugin]
+    })
+
+    expect(result.code.trimAll()).toEqual(`
+      const name = "bob";
+      const age = 18;
+      const desc = 'my name is ' + name + age;
     `.trimAll())
   })
 })
